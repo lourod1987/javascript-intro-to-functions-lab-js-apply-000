9703c9a902f057ec5d2a13bbb1297973cd071407
Luis Rodriguez III <louart87@gmail.com>
2018-06-05 03:44:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ function whisper(string) {
 
 
 function logShout(string) {
-    console.log(string);
+    console.log(string.toUpperCase());
 }
 
 
 function logWhisper(string) {
-    console.log(string);
+    console.log(string.toLowerCase());
 }
