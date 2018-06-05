31eead365cd9293235d5dda7ac5aeae000dd9b2c
Luis Rodriguez III <louart87@gmail.com>
2018-06-05 03:41:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ function shout(string) {
   return string.toUpperCase();
 }
 
+
 function whisper(string) {
   return string.toLowerCase();
 }
+
+
+function logShout(string) {
+  if (string === string.toUpperCase()) {
+    console.log(string);
+  }
+}
+
 
 function logShout(string) {
   if (string === string.toUpperCase()) {
